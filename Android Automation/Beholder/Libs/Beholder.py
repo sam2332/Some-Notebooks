45f894c16b22450c585ba17457fde802f2efc6bb
sam2332 <945622+sam2332@users.noreply.github.com>
2022-09-08 12:32:34 -0400

    updates

--- Android Automation/Beholder/Libs/Beholder.py
+++ Android Automation/Beholder/Libs/Beholder.py
@@ -185,16 +185,13 @@ class Beholder_Matcher:
 
 class Beholder_Image_Matcher(Beholder_Matcher):
     def __init__(self, name, layer, filename, threshhold=None,
-            sensitivity = None, convertToGray=True):
+            convertToGray=True):
         self.name = name
         self.layer = layer
         self.data = ""
         if threshhold is None:
             threshhold = 0.8
         self.threshhold = threshhold
-        if sensitivity is None:
-            sensitivity = 60
-        self.sensitivity = sensitivity
         if Path(filename).exists():
             self.data = PillowToCv2(Image.open(filename))
             if convertToGray:
@@ -206,42 +203,52 @@ class Beholder_Image_Matcher(Beholder_Matcher):
         result = cv2.matchTemplate(
             bh.layers[self.layer].data, self.data, method=cv2.TM_CCOEFF_NORMED
         )
-        min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
-
-        o={}
-        if max_val > self.threshhold:
-            height, width = self.data.shape[:2]
-            layer_offset_y = 0
-            layer_offset_x = 0
-            if bh.layers[self.layer].offsets is not None:
-                layer_offset_x, layer_offset_y = bh.layers[self.layer].offsets
-            top_left = max_loc
-            center = (
-                (layer_offset_x + top_left[0] + (width / 2)),
-                (layer_offset_y + top_left[1] + (height / 2)),
-            )
-
-            loc = np.where(result >= self.threshhold)
-
-            f = set()
-            for pt in zip(*loc[::-1]):
-                f.add(
-                    (
-                        round(layer_offset_x + pt[0] / self.sensitivity),
-                        layer_offset_y + round(pt[1] / self.sensitivity),
-                    )
+        loc = np.where(result >= self.threshhold)
+        o=defaultdict(list)
+        f = set()
+        
+        layer_offset_y = 0
+        layer_offset_x = 0
+        if bh.layers[self.layer].offsets is not None:
+            layer_offset_x, layer_offset_y = bh.layers[self.layer].offsets
+        for pt in zip(*loc[::-1]):
+            item = (
+                    round(layer_offset_x + pt[0] / self.data.shape[0]),
+                    round(layer_offset_y + pt[1] / self.data.shape[1]),
                 )
-            o[self.name]=BeholderMatch(self, center, f)
+            if item not in f:
+                f.add(
+                    item
+                ) 
+                
+                center = (layer_offset_x+pt[0],layer_offset_y+pt[1])
+                print("AddMatch",self.name, center)
+                if type(self.name) == str:
+                    o[self.name].append(BeholderMatch(self,self.layer, center,template_shape = self.data.shape))
+                elif type(self.name)==list:
+                    for name in self.name:
+                        o[name].append(BeholderMatch(self,self.layer, center,template_shape = self.data.shape))
         return o
 
 # In[ ]:
 
 class BeholderMatch():
-    def __init__(self,matcher,center,f):
+    def __init__(self,matcher,layer,center,template_shape):
         self.matcher = matcher
+        self.layer = layer
         self.center = center
-        self.f = f
-    
+        self.template_shape = template_shape
+        self.color = (255, 0, 0)
+        self.thickness = 2
+    def show(self,bh):
+        img = bh.layers[self.layer].data
+        center = (
+            self.center[0]-bh.layers[self.layer].offsets[0],
+            self.center[1]-bh.layers[self.layer].offsets[1]
+        )
+        img = cv2.rectangle(img, (center[0], center[1]),
+                            (center[0]+self.template_shape[1], center[1]+self.template_shape[0]), self.color, self.thickness)
+        display(Image.fromarray(img))    
 
 
 
@@ -259,11 +266,14 @@ class Beholder:
         self.layer_modifiers[ch.name] = ch
 
     def addMatcher(self, m):
-        self.matchers[m.name] = m
+        if type(m.name) ==str:
+            self.matchers[m.name] = m
+        else:
+            self.matchers['_'.join(m.name)] = m
 
     def readNextImage(self):
         self.layers["image"] = Beholder_Layer(
-            name="image", data=PillowToCv2(self.generator())
+            name="image", data=PillowToCv2(self.generator()),offsets=(0,0)
         )
 
     def digestImage(self):
@@ -303,6 +313,7 @@ class Beholder_Layer_Chopper_Grayscale(Beholder_Layer_Chopper):
         o[self.name] = Beholder_Layer(
             name=self.name,
             data=cv2.cvtColor(bh.layers[self.from_layer].data, cv2.COLOR_BGR2GRAY),
+            offsets=bh.layers[self.from_layer].offsets
         )
         return o
 
@@ -327,6 +338,7 @@ class Beholder_Layer_Chopper_InRange(Beholder_Layer_Chopper):
         o[self.name] = Beholder_Layer(
             name=self.name,
             data=cv2.bitwise_and(hsv, bh.layers[self.from_layer].data, mask=mask),
+            offsets=bh.layers[self.from_layer].offsets
         )
         return o
 
